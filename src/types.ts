558b45aaaa9ec0bd916af770c02a8c0f33d3ae73
oberien <jaro.fietz@gmx.de>
2017-08-28 16:32:15 +0200

    Remove overlay, instead use colored border around card

--- src/types.ts
+++ src/types.ts
@@ -99,10 +99,12 @@ namespace Types {
         switch (type) {
             case CardType.BasegameMinor:
             case CardType.ExpansionMinor:
-                return "rgba(50, 50, 50, 0.3)";
+                // return "rgba(50, 50, 50, 0.3)";
+                return "saddlebrown";
             case CardType.BasegameMajor:
             case CardType.ExpansionMajor:
-                return "rgba(255, 255, 0, 0.25)";
+                // return "rgba(255, 255, 0, 0.25)";
+                return "goldenrod";
             case Unique.ASpreadOfRampantGreen:
             case Unique.BringerOfDreamsAndNightmares:
             case Unique.LightngingsSwiftStrike:
@@ -113,7 +115,9 @@ namespace Types {
             case Unique.VitalStrengthOfTheEarth:
             case Unique.SharpFangsBehindTheLeaves:
             case Unique.KeeperOfTheForbiddenWilds:
-                return "rgba(0, 128, 0, 0.25)";
+                // return "rgba(0, 128, 0, 0.25)";
+                // return "darkolivegreen";
+                return "#6B8E23";
             default:
                 throw new Error();
         }
@@ -160,13 +164,14 @@ namespace Types {
             front.appendChild(img);
             let overlay = <HTMLDivElement> document.createElement("div");
             overlay.style.position = "absolute";
-            overlay.style.backgroundColor = toColor(this.type);
-            overlay.style.width = "67%";
-            overlay.style.height = "10%";
-            overlay.style.left = "27%";
-            overlay.style.top = "4%";
+            overlay.style.border = "15px solid";
+            overlay.style.borderColor = toColor(this.type);
+            overlay.style.width = "90%";
+            overlay.style.height = "93%";
+            overlay.style.left = "0";
+            overlay.style.top = "0";
             overlay.style.zIndex = "1";
-            overlay.style.borderRadius = "0 13px 0 0";
+            overlay.style.borderRadius = "20px";
             front.appendChild(overlay);
 
             let back = document.createElement("div");
